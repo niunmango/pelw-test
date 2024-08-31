063b79ddec74d095d9d891284f3d6d7f6fce4df8
Ramiro Garcia <niunmango@yahoo.com>
2024-08-31 18:03:51 -0300

    actions css

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 function toggleElementVisibility() {
     const element = document.getElementById("hiddenElement");
-    element.style.display = element.style.display === "none" ? "block" : "none";
+    element.style.display = "block";
   }
