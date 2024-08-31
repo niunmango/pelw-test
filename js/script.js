aa90a1c3cb86653c2d5dc0664bca7413fdaea810
Ramiro Garcia <niunmango@yahoo.com>
2024-08-31 17:48:10 -0300

    Cambios iniciales

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function toggleElementVisibility() {
+    const element = document.getElementById("hiddenElement");
+    element.style.display = element.style.display === "none" ? "block" : "none";
+  }
